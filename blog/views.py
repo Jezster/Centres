9c2c832a45d1c1bf4b2fb8fde97e94d9bb7c1375
Jeremy Ward (Europe) <jeremy.ward@global.ntt>
2021-04-19 10:00:25 +0100

    Post update complete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView
+)
 
 # Create your views here.
 
@@ -11,5 +18,32 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+
